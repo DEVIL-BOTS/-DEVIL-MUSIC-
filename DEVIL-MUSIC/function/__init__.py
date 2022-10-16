77dbfce5291ca15747b697d1fbffb043ed97db5b
DevilBotz <110766400+DEVIL-BOTS@users.noreply.github.com>
2022-10-16 22:02:56 +0530

    Create __init__.py

---
+++ DEVIL-MUSIC/function/__init__.py
@@ -0,0 +1,5 @@
+from DEVIL-MUSIC.function.admins import admins
+from DEVIL-MUSICDEVIL-MUSIC.function.admins import get
+from DEVIL-MUSIC.function.admins import set
+
+__all__ = ["set", "get", "admins"]
