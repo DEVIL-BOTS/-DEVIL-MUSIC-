aa90508a34658a5f00f620cdf9413ab5bc4961d1
DevilBotz <110766400+DEVIL-BOTS@users.noreply.github.com>
2022-10-16 22:48:01 +0530

    Create __init__.py

---
+++ DEVIL-MUSIC/services/queues/__init__.py
@@ -0,0 +1,7 @@
+from DEVIL-MUSIC.services.queues.queues import clear 
+from DEVIL-MUSIC.services.queues.queues import get
+from DEVIL-MUSIC.services.queues.queues import is_empty
+from DEVIL-MUSIC.services.queues.queues import put
+from DEVIL-MUSIC.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
